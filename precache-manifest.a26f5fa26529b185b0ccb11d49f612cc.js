78f4d820d388af1bd215cddd15c384eb6d298723
ejjoj <szymaskibartosz@gmail.com>
2020-09-23 19:48:25 +0200

    Updates

---
+++ precache-manifest.a26f5fa26529b185b0ccb11d49f612cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b774f64fee055e1ff37",
+    "url": "/static/css/main.fd20425f.chunk.css"
+  },
+  {
+    "revision": "4b774f64fee055e1ff37",
+    "url": "/static/js/main.f93efd52.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73108127183b432dfcb2",
+    "url": "/static/js/2.d78fdad7.chunk.js"
+  },
+  {
+    "revision": "3af0c5f92e02e9a746bdcc55308cd4cb",
+    "url": "/static/media/ja.3af0c5f9.jpg"
+  },
+  {
+    "revision": "cdf5b5e1a8dd51c1130b4d4a86048b8a",
+    "url": "/static/media/PHP-logo.cdf5b5e1.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/static/media/Unofficial_JavaScript_logo_2.dd60b11e.svg"
+  },
+  {
+    "revision": "bb3b26211b3a981b358994eddaf63fc1",
+    "url": "/static/media/AJAX_logo_by_gengns.bb3b2621.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/React-icon.05a3e30f.svg"
+  },
+  {
+    "revision": "b04e488e156e32b474e695c43c9e7ac4",
+    "url": "/static/media/mysql-ar21.b04e488e.svg"
+  },
+  {
+    "revision": "5a76390c7f7e8b68f245cd6fdcfeb5e0",
+    "url": "/index.html"
+  }
+];
